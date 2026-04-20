4932f299af219f734ed6f3fe5bbfc5a836f3299e
CodeVoyager0210 <2351584@tongji.edu.cn>
2026-04-21 00:36:02 +0800

    Fix: basePath + assetPrefix + organize files to subfolder

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/love-miumiu',
+  assetPrefix: '/love-miumiu',
   trailingSlash: true,
   images: {
     unoptimized: true,
