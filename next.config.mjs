679aa977b69ab008118f24f70093492227e3316c
CodeVoyager0210 <2351584@tongji.edu.cn>
2026-04-21 00:28:46 +0800

    Simplify: remove basePath to test

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/love-miumiu',
-  assetPrefix: '/love-miumiu',
   trailingSlash: true,
   images: {
     unoptimized: true,
